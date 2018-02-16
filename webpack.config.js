0b4c470014aad3754b3f17300613f5419731e632
Natalia <nkapelist@gmail.com>
2018-02-16 17:56:15 +0200

    add node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ const path = require('path');
 module.exports = {
 	entry: {
         main: path.resolve(__dirname,'src','index.js'),
-        main: path.resolve(__dirname,'src','posts.js'),
-
+        main: path.resolve(__dirname,'src','posts.js')
 	},
 	output: {
 		filename: '[name].js',
